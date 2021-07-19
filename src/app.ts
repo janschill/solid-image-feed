8c4bf89fe1aec43710a8e89b899ed569b535272d
Jan Schill <janschill@me.com>
2021-07-19 16:47:53 +0200

    Serve static files from public dir

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,10 @@ const port = 3000;
 
 const IMAGE_FEED_URL = "https://janschill.net/public/solid-image-feed-example/"
 
+app.use(express.static('public'))
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 app.listen(port, () => {
   return console.log(`Server is listening on ${port}`);
